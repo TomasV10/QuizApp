4c04fbe2dd209b67fa017ebdd67f23650c1cbc6f
TomasV10 <tomasvarnauskas19@gmail.com>
2020-05-18 18:22:14 +0300

    Created entity classes

---
+++ src/main/java/com/practise/quiz/entities/Answer.java
@@ -0,0 +1,68 @@
+package com.practise.quiz.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "answer")
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "content", nullable = false, length = 65535)
+    private String content;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+    @Column(name = "correct", nullable = false)
+    private boolean correct;
+
+    public Answer() {
+    }
+
+    public Answer(Question question, String content,  boolean correct) {
+        this.content = content;
+        this.question = question;
+        this.correct = correct;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", correct=" + correct +
+                '}';
+    }
+}
---
+++ src/main/java/com/practise/quiz/entities/Question.java
@@ -0,0 +1,63 @@
+package com.practise.quiz.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "question")
+public class Question implements java.io.Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "content", nullable = false, length = 65535)
+    private String content;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "question")
+    private List<Answer> answers = new ArrayList<>();
+
+    public Question() {
+    }
+
+    public Question(String content) {
+        this.content = content;
+    }
+
+    public Question(String content, List<Answer> answers) {
+        this.content = content;
+        this.answers = answers;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", answers=" + answers +
+                '}';
+    }
+}
