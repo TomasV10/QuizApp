6a253e78e0327c8b7cb2509a77511ff3d46946e8
TomasV10 <tomasvarnauskas19@gmail.com>
2020-05-19 17:48:30 +0300

    added @Bean

--- src/main/java/com/practise/quiz/QuizApplication.java
+++ src/main/java/com/practise/quiz/QuizApplication.java
@@ -1,13 +1,20 @@
 package com.practise.quiz;
 
+import Services.QuestionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+
 public class QuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizApplication.class, args);
 	}
 
+	@Bean
+	public QuestionService questionService(){
+		return new QuestionService();
+	}
 }
